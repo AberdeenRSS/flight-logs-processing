aa4c201643993f4a1578d34be0f96bd41951a4cc
alanjsfleming <alanjsfleming@gmail.com>
2024-05-23 21:46:40 +0100

    added new files and filtered from flight start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
 
 flight_file_path = "flight4maycorrect"
 file_path = flight_file_path + "/" + flight_file_path + "_output.csv"
@@ -18,14 +19,17 @@ df.columns = new_headers
 print(new_headers)
 #print(df.iloc[0])
 
+# print all unique entries in df['state']
+print(df['state'].unique())
+
 if 'timestamp' in df.columns and 'countdown' in df.columns:
     # Convert 'timestamp' to a datetime object if it's not already in that format
     df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
 
     # Find the first row where countdown is not null
-    first_valid_index = df['countdown'].first_valid_index()
-
-    first_ignited_index = df['Ignited'].first_valid_index()
+    
+    # find first row where state = flight
+    first_ignited_index = df[df['state'] == 'flight'].index[0]
 
     # Slice the DataFrame from the first valid index to the end
     df = df.loc[first_ignited_index:]
@@ -33,9 +37,15 @@ if 'timestamp' in df.columns and 'countdown' in df.columns:
 
     # Plotting countdown against timestamp
     plt.figure(figsize=(10, 5))
-    plt.plot(df['timestamp'], df['position-x'], marker="o", linestyle='-')
+    plt.plot(df['timestamp'], df['acceleration-z'], marker="o", linestyle='-')
+   
+    # Format x-axis to display time in minutes and seconds
+    time_format = DateFormatter("%M:%S")
+    plt.gca().xaxis.set_major_formatter(time_format)
+
 
     plt.grid(True)
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
+
